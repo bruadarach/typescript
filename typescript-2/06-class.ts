22fbf326dcf9d6db391387d66af42fd29a0c471b
Sujeong Ji <suji.sujeongji@gmail.com>
2022-10-13 23:58:00 +0900

    typescript

--- typescript-2/06-class.ts
+++ typescript-2/06-class.ts
@@ -28,4 +28,32 @@ m1.delegateWork();
 m1.greet();
 console.log(m1.employeeName);
 
-/* Access Modifier */
+/* Access Modifier 
+ - public : for free accessibility
+ - private : only accessible within the class
+  - protected : only accessible within the class and its derived class
+*/
+class Employee {
+  private employeeName: string;
+  constructor(name: string) {
+    this.employeeName = name;
+  }
+  greet() {
+    console.log(`Good Morning ${this.employeeName}`);
+  }
+}
+
+let emp1 = new Employee("Sujeong Ji");
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+  constructor(managerName: string) {
+    super(managerName);
+  }
+  delegateWork() {
+    console.log(`Manager delegating tasks`);
+    // can't use ${this.employeeName} in derived class
+    // if you change private to protected, you can acccess to ${this.employeeName}
+  }
+}
